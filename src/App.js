c9093f01a61270b52f4af126898f5ab9403c4688
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-12 19:30:52 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,85 @@
 import web3 from "./web3"
+import lottery from "./lottery"
+import {Component} from "react"
 
-const App = () => {
-  return (
-    <h1>Wow</h1>
-    )
+class App extends Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: ""
+  }
+  
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call()
+    const players = await lottery.methods.getPlayers().call()
+    const balance = await web3.eth.getBalance(lottery.options.address)
+    this.setState({manager, players, balance})
+  }
+  
+  onSubmit = async (event) => {
+    event.preventDefault()
+    const accounts = await web3.eth.getAccounts()
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether")
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by {this.state.manager}</p>
+        <p>There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!</p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <input value={this.state.value} placeholder="Amount in ether" onChange={event => this.setState({value: event.target.value})} />
+          </div>
+          <br />
+          <button>Enter</button>
+        </form>
+      </div>
+      )
+  }
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
